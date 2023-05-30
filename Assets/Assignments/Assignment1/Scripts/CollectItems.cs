c260150ab5c9acf2bafa266ff213c89e4cda6796
Tero Korpela <tero.korpela@gmail.com>
2023-05-30 17:11:33 +0300

    Assignment2

--- Assets/Assignments/Assignment1/Scripts/CollectItems.cs
+++ Assets/Assignments/Assignment1/Scripts/CollectItems.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class CollectItems : MonoBehaviour
     private static int animStateID = Animator.StringToHash("AnimationState");
     private NavMeshAgent agent;
     private Animator animator;
+    [SerializeField][Range(0f, 2f)] private float pickupDistance = .5f;
     public bool AnimationDone { private get; set; }
 
 
@@ -26,13 +28,16 @@ public class CollectItems : MonoBehaviour
         foreach (var i in items)
         {
             // Setting destination to navmesh agent
+            agent.isStopped = false;
+            agent.stoppingDistance = pickupDistance;
             agent.SetDestination(i.transform.position);
 
             // Starting walk animation
             animator.SetInteger(animStateID, (int)AnimationStates.WALK);
 
             // Wait until agent stops
-            yield return new WaitUntil(() => agent.hasPath == false && agent.pathPending == false);
+            yield return new WaitUntil(() => agent.hasPath == true && agent.pathPending == false && agent.remainingDistance < pickupDistance);
+            agent.isStopped = true;
 
             // Pickup item animation
             animator.SetInteger(animStateID, (int)AnimationStates.CROUCH);
